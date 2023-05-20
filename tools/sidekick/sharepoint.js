d07f403c564c7300761d6dec996c2242010e3240
anagarwa <anagarwa@adobe.com>
2023-05-21 03:16:12 +0530

    updating sharepoint.js

--- tools/sidekick/sharepoint.js
+++ tools/sidekick/sharepoint.js
@@ -240,7 +240,7 @@ export async function checkAndUpdateExcelFile() {
     };
 
     const fileId = await getFileId(folderPath,filename);
-    await addEntriesToExcel(fileId, sheetName, entry);
+    //await addEntriesToExcel(fileId, sheetName, entry);
     await findTextInExcel(fileId, sheetName, entry.id);
      //await createFolder(folderPath);
      //await createNewExcelFile();
