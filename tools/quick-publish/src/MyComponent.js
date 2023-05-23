3919032d6b4e1a055f3cce09c4af71967fec7573
anagarwa <anagarwa@adobe.com>
2023-05-23 14:16:47 +0530

    updating sharepoint.js

--- tools/quick-publish/src/MyComponent.js
+++ tools/quick-publish/src/MyComponent.js
@@ -11,9 +11,17 @@ const MyComponent = () => {
     return <div>This is my component</div>;
 };
 
-const myFunction = () => {
+const myFunction = async () => {
     // Your JavaScript logic goes here
     console.log('My function is running1234!');
+    try {
+        // Perform async operations
+        const result = await fetch('https://example.com/api/data');
+        const data = await result.json();
+        console.log('Async function completed:', data);
+    } catch (error) {
+        console.error('Error in async function:', error);
+    }
 };
 
 export default MyComponent;
