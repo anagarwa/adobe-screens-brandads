46b8306cd82c4229590c747ea20aba0deec9b339
anagarwa <anagarwa@adobe.com>
2023-05-24 23:45:22 +0530

    updating

--- tools/quick-publish/src/sharepoint.js
+++ tools/quick-publish/src/sharepoint.js
@@ -75,40 +75,107 @@ function getRequestOption() {
 
 
 export async function PublishAndNotify() {
-    console.log("in publish and notify");
-    validateConnnection();
-
-    const entries = [{
-        id: 'uniqueid1',
-        notify: 'changes',
-        sent: 'yes'
-    },
-    {
-        id: 'uniqueid2',
-        notify: 'goal',
-        sent: 'yes'
-    },
-    ];
-
-    const  entryUpdated = await addEntriesToExcel(entries);
+    // console.log("in publish and notify");
+    // validateConnnection();
+    //
+    // const entries = [{
+    //     id: 'uniqueid1',
+    //     notify: 'changes',
+    //     sent: 'yes'
+    // },
+    // {
+    //     id: 'uniqueid2',
+    //     notify: 'goal',
+    //     sent: 'yes'
+    // },
+    // ];
+    //
+    // const  entryUpdated = await addEntriesToExcel(entries);
     const quickPublish = await quickpublish();
-    if ("updated" === entryUpdated && quickPublish === "published") {
+    // if ("updated" === entryUpdated && quickPublish === "published") {
+    //     return "updated";
+    // } else {
+    //     return  "not updated";
+    // }
+
+    if (quickPublish === "published") {
+        console.log("published");
         return "updated";
-    } else {
-        return  "not updated";
     }
 
 }
 
+// async function quickpublish() {
+//     console.log("in quick publish1");
+//     validateConnnection();
+//
+//     const options = getRequestOption();
+//     options.method='POST';
+//     const response = await fetch(`https://admin.hlx.page/preview/hlxsites/fcbayern/main/de/spiele/profis/bundesliga/2022-2023/sv-werder-bremen-fc-bayern-muenchen-06-05-2023/liveticker`, options);
+//     if (response.ok) {
+//         console.log("published");
+//         return "published";
+//     }
+// }
+
 async function quickpublish() {
     console.log("in quick publish1");
-    validateConnnection();
-
-    const options = getRequestOption();
-    options.method='POST';
+    // validateConnnection();
+    //
+    const options = {
+        method:'POST'
+    }
     const response = await fetch(`https://admin.hlx.page/preview/hlxsites/fcbayern/main/de/spiele/profis/bundesliga/2022-2023/sv-werder-bremen-fc-bayern-muenchen-06-05-2023/liveticker`, options);
     if (response.ok) {
         console.log("published");
+        const livetickerurl = `https://${branch}--${repoName}--${orgName}.hlx.page${path}`;
+        console.log(`liveticker url is ${livetickerurl}`);
+
+        // const browser = await puppeteer.launch();
+        // const page = await browser.newPage();
+        // await page.goto(livetickerurl);
+        // const liveTickerHtml = await page.content();
+        // await browser.close();
+
+        const liveTickerResponse = await fetch(livetickerurl);
+        const liveTickerHtml = await liveTickerResponse.text();
+        console.log(liveTickerHtml);
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(liveTickerHtml, 'text/html');
+
+
+        const jsonArray = [];
+        const eventElements = doc.querySelectorAll('.goal, .whistle');
+        for (let j = 0; j < eventElements.length; j++) {
+            const eventElement = eventElements[j];
+            const jsonObject = {}
+            jsonObject['eventType'] = eventElement.classList;
+            const divElements = eventElement.querySelectorAll(':scope > div');
+            for (let i = 0; i < divElements.length; i++) {
+                console.log(divElements[i]);
+                const keyValueDiv = divElements[i].querySelectorAll('div');
+                const key = keyValueDiv[0].textContent.trim().toLowerCase().replace(' ', '_');
+                const value = keyValueDiv[1].textContent.trim();
+                jsonObject[key] = value;
+            }
+            if (jsonObject['push'] === 'yes' || jsonObject['push'] === 'no') {
+                //todo method options with actual service
+                // var options = {
+                //     method:'POST',
+                //     body:JSON.stringify(jsonObject)
+                // }
+                const notificationSent = await fetch("https://liveticker1--fcbayern--hlxsites.hlx.page/tools/quick-publish/dist/notification.html", {});
+                const notifcalresponse = await notificationSent.text();
+                console.log("response is " + notifcalresponse);
+                if (notificationSent.ok) {
+                    console.log("notification has been updated");
+                }
+                //todo code to confirm if it has been updated in excel if not send notification and update excel
+                jsonArray.push(jsonObject);
+            }
+        }
+
+        console.log(JSON.stringify(jsonArray));
         return "published";
     }
 }
