665c0edb29f234e68a4b78a7258eb9f470923f11
anagarwa <anagarwa@adobe.com>
2023-05-29 23:44:23 +0530

    sharepoint site

--- tools/sidekick/sharepoint.js
+++ tools/sidekick/sharepoint.js
@@ -365,15 +365,19 @@ async function downloadUploadDocumentOnSite(sitesid, documentid) {
         validateConnnection();
 
         const options = getRequestOption();
+        options.method = 'GET';
         options.headers.append('Content-Type', 'application/json');
 
         // options.method='GET';
         // options.
-        const url = `https://graph.microsoft.com/v1.0/sites/${siteId}`
+        //const url = `https://graph.microsoft.com/v1.0/sites/${siteId}`
+
+        const siteUrl = "https://adobe.sharepoint.com/sites/FirstSite/Shared%20Documents/Forms/AllItems.aspx";
+        const url = `https://graph.microsoft.com/v1.0/sites?filter=webUrl eq '${siteUrl}'`;
         const response = await fetch(url, options);
         if (response.ok) {
             const responseData = await response.json();
-            console.log(responseData);
+            console.log(responseData.value[0].id);
         }
 
     } catch (error) {
