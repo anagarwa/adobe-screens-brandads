3e7af7ac73b44235ba68b816c165f7027494ae82
anagarwa <anagarwa@adobe.com>
2023-07-26 14:51:45 +0530

    garage week

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,16 +1,16 @@
 
 export default async function decorate(block) {
     const rows = [...block.children];
-    var header = document.querySelector('header'); // Select the header element
+    const header = document.querySelector('header');
     if (header) {
-        header.parentNode.removeChild(header); // Remove the header element from its parent node
+        header.remove();
     }
 
-    var footer = document.querySelector("footer"); // Select the header element
+    const footer = document.querySelector("footer");
     if (footer) {
-        footer.parentNode.removeChild(footer); // Remove the header element from its parent node
+        footer.remove();
     }
-
+    block.textContent = '';
     rows.forEach((row) => {
         const img = document.createElement('img');
         img.src = row.children[1].innerText.trim();;
@@ -18,25 +18,20 @@ export default async function decorate(block) {
         block.appendChild(img);
     });
 
-    document.addEventListener('DOMContentLoaded', function() {
+    let currentIndex = 0;
 
-        let currentIndex = 0;
+    // Function to show the next image
+    function showNextImage() {
+        currentIndex = (currentIndex + 1) % rows.length;
+        updateCarousel();
+    }
 
-        // Function to show the next image
-        function showNextImage() {
-            currentIndex = (currentIndex + 1) % rows.length;
-            updateCarousel();
-        }
+    // Function to update the carousel position
+    function updateCarousel() {
+        block.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
 
-        // Function to update the carousel position
-        function updateCarousel() {
-            block.style.transform = `translateX(-${currentIndex * 100}%)`;
-        }
-
-        // Set an interval to automatically show the next image every 3 seconds
-        setInterval(showNextImage, 1000);
-    });
-
-
+    // Set an interval to automatically show the next image every 3 seconds
+    setInterval(showNextImage, 1000);
 
 }
